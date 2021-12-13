daa4eeed08ac6e0b7ac8554cc7f2efaf7e282dfb
Mhdiyekaffash <91253157+Mhdiyekaffash@users.noreply.github.com>
2021-12-13 17:58:03 +0330

    The first part of the menu

---
+++ Ticket/main.py
@@ -0,0 +1,29 @@
+#    In the name of God ❤❤❤❤
+
+# import Administrator_user
+# import Happen
+
+special_code = 'SniMda1309'  # Special code for admins
+introduction = input("who are you ? 1.Admin user or 2.client ! ")
+if introduction == '1':
+    login_admin = input("\n1.Register\n2.login ?")
+    if login_admin == '1':
+        """
+        ----> Call method add_administrator_user of module Administrator
+        """
+    if login_admin == '2':
+        for i in range(1, 4):
+            Admin_password = input("please Enter the admin password : ")
+            if Admin_password == special_code:
+                print("The password is correct ✔ welcome.")
+                """
+                ---> Call method log_in of module Administrator 
+                *** The event definition operation is written in this section
+                """
+                break
+            else:
+                if i == 3:
+                    print("sorry ! Your access to the site has been blocked")
+                else:
+                    print(f"The password is incorrect ❌ You have {3-i} more chances")
+
