2a2e56f9e52afbd7f9f1fa3014c1263978ae86d5
Mhdiyekaffash <91253157+Mhdiyekaffash@users.noreply.github.com>
2021-12-13 17:56:34 +0330

    add regexs and Check that the password is correct

--- Ticket/Administrator_user.py
+++ Ticket/Administrator_user.py
@@ -1,29 +1,21 @@
 import logging
+import re
 from File_Handler import FileHandler
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 # create handlers and set level
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(level=logging.INFO)
 file_handler = logging.FileHandler('Administrator_user')
-file_handler.setLevel(level=logging.WARNING)
+file_handler.setLevel(level=logging.INFO)
 # create formatters and add it to handlers
-stream_format = logging.Formatter('%(asctime)s ::%(levelname)s - %(name)s - %(filename)s - %(message)s')
 file_format = logging.Formatter('%(asctime)s ::%(levelname)s - %(filename)s - %(message)s')
-stream_handler.setFormatter(stream_format)
 file_handler.setFormatter(file_format)
 # add handlers to the logger
-logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
-logger.debug('this is a debug message')
-logger.info('this is an info message')
-logger.warning('this is an warning message')
-logger.error('this is an error message')
-logger.critical('this is an critical message')
 
 
 class Administrator_user:
+    special_code = 'SniMda1309'
     list_Administrator_user = []  # for all Administrator user
 
     def __init__(self, user_name, password, name, l_name, national_code, phone_number, email_address):
@@ -46,44 +38,86 @@ class Administrator_user:
     def add_administrator_user(cls):  # add new account for administrator_user
         name = input("please enter your First name :")
         l_name = input("please enter your Last name :")
+        phone_number = int(input("please enter your phone_number :"))
+        while not cls.check_phone_number(phone_number):
+            phone_number = int(input("The phone_number is incorrect. please enter again :"))
+        national_code = int(input("please enter your national_code :"))
+        while not cls.check_national_code(national_code):
+            national_code = int(input("The national_Code is incorrect, Must be 10 digits. please enter again :"))
+        email_address = input("please enter your email_address :")
+        while not cls.check_email(email_address):
+            email_address = input("The address email is incorrect, put '@' in the email. Please enter again :")
         user_name = input("please enter your user_name :")
         password = input("please enter your password :")
         re_pass = input("please Re-enter your password :")
-        # چک شود حتما دو پسورد یکی باشن
-        phone_number = int(input("please enter your phone_number :"))
-        national_code = int(input("please enter your national_code :"))
-        email_address = input("please enter your email_address :")
+        while not Administrator_user.check_pass(password, re_pass):
+            print("❌ The two passwords entered do not match, please enter again ❌")
+            password = input("password :")
+            re_pass = input("Re_password :")
         id = cls.generate_id(user_name, password)
         cls.list_Administrator_user.append(id)
-
+        print(f"Your registration was completed successfully. Special code for admin is {cls.special_code} "
+              f"Please remember .")
         return name, l_name, user_name, password, re_pass, phone_number, national_code, email_address
 
-    def check_email(self, email_address):
-        print("To check the correctness of the email")
+    @staticmethod
+    def check_pass(password, re_pass):
+        if password != re_pass:
+            return False
+        else:
+            return True
 
-    def check_national_code(self, national_code):
-        print("To check the correctness of the national_code")
+    @staticmethod
+    def check_email(email_address):
+        regex1 = r"(.+\@.+\.[c][o][m])"
+        matches1 = re.search(regex1, email_address)
+        if matches1:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def check_national_code(national_code):
+        regex2 = r"(/^[0-9]{10}$/g)"
+        matches2 = re.search(regex2, national_code)
+        if matches2:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def check_phone_number(phone_number):
+        regex3 = r"(/^(0|84)(2(0[3-9]|1[0-6|8|9]|2[0-2|5-9]|3[2-9]|4[0-9]|5[1|2|4-9]|6[0-3|9]|7[0-7]" \
+                 r"|8[0-9]|9[0-4|6|7|9])|3[2-9]|5[5|6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])([0-9]{7})$/mg)"
+        matches3 = re.search(regex3, phone_number)
+        if matches3:
+            return True
+        else:
+            return False
 
     @classmethod
     def log_in(cls):
         """
-        این متد نام کاربری و پسورد رو میگیره و چک میکنه که ایا صحیح است یا خیر
+        This method takes the username and password and checks if it is correct or not
         """
-        user_name = input("please enter your user_name :")
-        password = input("please enter your password :")
-
-        id = Administrator_user.generate_id(user_name, password)  # braye inke check konim ghablan sabtnam shode ya na
-        try:
+        for i in range(1, 4):
+            user_name = input("please enter your user_name :")
+            password = input("please enter your password :")
+            id = Administrator_user.generate_id(user_name, password)
             if id in Administrator_user.list_Administrator_user:
-                print("میتونید وارد بشید")
+                print("welcome ✔")
+                break
             else:
-                raise Exception
-        except Exception as E:
-            logger.error("expection occured here !", exc_info=True)
-            print("There is no user by this name, please try again")
-            # if bishtar az 3 bar eshtebah vared kard ---> logger.critical("expection occured here !", exc_info=True)
+                if i == 3:
+                    print("Your account has been locked 🔒")
+                    logger.warning(f"Account locking !", exc_info=True)
+                else:
+                    print(f"The user_name or password is incorrect ❌ You have {3 - i} more chances")
 
-    def file_admin(self):  # for submit names Administrator_user
+    def file_admin(self):
+        """
+        for submit and edit file
+        :return:
+        """
         print("for submit names Administrator_user ")
-
 
